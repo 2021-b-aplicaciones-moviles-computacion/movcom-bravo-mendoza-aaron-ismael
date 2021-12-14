06c125684718d5c7d9583e7e8e71112b93da087c
Aarón Bravo <aaron.bravo@epn.edu.ec>
2021-12-14 15:58:18 -0500

    hasta intents

--- 01-android/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ 01-android/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+    val CODIGO_RESPUESTA_INTENT_EXPLICITO = 401
+    val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+        if(result.resultCode == Activity.RESULT_OK){
+
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,7 +28,21 @@ class MainActivity : AppCompatActivity() {
         botonListView.setOnClickListener {
             irActividad(BListView::class.java)
         }
+        val botonIntent = findViewById<Button>(R.id.btn_intent)
+        botonIntent.setOnClickListener {
+            abrirActividadConParametros(CIntentExplicitoParametros::class.java)
+        }
 
+    }
+
+    fun abrirActividadConParametros(clase: Class<*>,){
+        val intentExplicito = Intent(this, clase)
+        //solo variables primitivas
+        intentExplicito.putExtra("nombre", "Aaron")
+        intentExplicito.putExtra("apellido", "Bravo")
+        intentExplicito.putExtra("edad", "29")
+        resultLauncher.launch(intent)
+        //startActivityForResult(intent, CODIGO_RESPUESTA_INTENT_EXPLICITO)
     }
 
     fun irActividad(clase: Class<*>,){
